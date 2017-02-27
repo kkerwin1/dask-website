9cb4c205f290f36d7ac9f682316657693f4a3e88
kkerwin1 <kkerwin1@gmail.com>
2017-02-26 19:50:12 -0600

    Completed tutorial #5

--- dask_website/polls/tests.py
+++ dask_website/polls/tests.py
@@ -20,6 +20,33 @@ def create_question(question_text, days):
         question_text=question_text, pub_date=time
     )
 
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+
+        future_question = create_question(
+            question_text="Future question.", days=5
+        )
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+
+        past_question = create_question(
+            question_text="Past Question.", days=-5
+        )
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
 class QuestionViewTests(TestCase)
     def test_index_view_with_no_questions(self):
         """
