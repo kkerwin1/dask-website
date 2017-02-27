17b082f836bf85beb5d54cf4871053a25c3dcad8
Kristopher H Kerwin <kris@gimli>
2017-02-26 19:27:02 -0600

    Tutorial 5: Views Tests

--- dask_website/polls/views.py
+++ dask_website/polls/views.py
@@ -10,13 +10,15 @@ from .models import Choice, Question
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
-    
+
     def get_queryset(self):
         """
         Return the last five published questions.
         """
-        
-        return Question.objects.order_by("-pub_date")[:5]
+
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
