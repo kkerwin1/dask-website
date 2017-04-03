1d2ec85f67788403c53ccc461790a5480d6e88f1
kkerwin1 <kkerwin1@gmail.com>
2017-04-02 19:23:42 -0500

    starting over

--- dask_website/dask_website/urls.py
+++ dask_website/dask_website/urls.py
@@ -1,18 +1,21 @@
-from django.conf.urls.defaults import *
-import registration
+"""dask_website URL Configuration
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^dj_project/', include('dj_project.foo.urls')),
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
 
-    # Uncomment this for admin:
-    (r'^admin/', include('django.contrib.admin.urls')),
-    (r'^dj_survey/', include('dj_project.dj_survey.urls')),
-    (r'^accounts/', include('registration.urls')),
-    (r'^yui/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': '../../svn_views/yui/build','show_indexes': True
-    }),
-    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': './media','show_indexes': True
-    }),
-)
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
