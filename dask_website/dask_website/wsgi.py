afae48c69bc9f7fa5c838e72e523a4c991578f51
kkerwin1 <kkerwin1@gmail.com>
2017-04-08 00:41:27 -0500

    starting from mezzanine base project

--- dask_website/dask_website/wsgi.py
+++ dask_website/dask_website/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from mezzanine.utils.conf import real_project_name
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dask_website.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "%s.settings" % real_project_name("dask_website"))
 
 application = get_wsgi_application()
